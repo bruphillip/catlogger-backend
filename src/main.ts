0976fbf71f6de69405659c384204c7a1e8cc49ac
Bruno Philippe <bruni.felipe@hotmail.com>
2022-11-08 11:32:06 -0300

    package json cleanup / fastfy adapter / readme cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
-// eslint-disable-next-line import-helpers/order-imports
-import * as dotenv from 'dotenv'
-dotenv.config()
-
 import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 import env from 'constant/env'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  )
   app.enableCors()
-  await app.listen(env.APP_PORT)
+  await app.listen(env.APP_PORT, '0.0.0.0')
 }
 bootstrap()
